00d1e0d30e2b7877b873099fd50092f02121a29c
shezneu <116790413+shezneu@users.noreply.github.com>
2022-10-27 03:07:06 -0400

    Delete src directory

--- src/main/resources/db/migration/V1.2__create_tbl_consumer.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists consumer
-(
-    id                            bigint primary key auto_increment,
-    email                         varchar(70) not null,
-    identification_no             varchar(50) not null,
-    identification_type           varchar(10) not null,
-    consent_given                 bool,
-    terms_and_conditions_accepted bool
-)
